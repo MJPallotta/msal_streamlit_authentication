5d20aa865ad7a11833c58a26b0062a5384d61456
Matt Pallotta <matthew.pallotta@independenthealth.com>
2025-11-12 13:16:23 -0500

    reformat code

--- msal_streamlit_authentication/frontend/src/auth/msal-auth.tsx
+++ msal_streamlit_authentication/frontend/src/auth/msal-auth.tsx
@@ -1,34 +1,46 @@
-import {BrowserAuthOptions, CacheOptions, LogLevel, PublicClientApplication} from "@azure/msal-browser";
+import {
+  BrowserAuthOptions,
+  CacheOptions,
+  LogLevel,
+  PublicClientApplication,
+} from '@azure/msal-browser';
 
-export const useMsalInstance = function (auth_config: BrowserAuthOptions, cache_config: CacheOptions) {
-    const instance = new PublicClientApplication({
-        auth: auth_config,
-        cache: cache_config,
-        system: {
-            loggerOptions: {
-                loggerCallback: (level: LogLevel, message: string, containsPii: any) => {
-                    if (containsPii) {
-                        return;
-                    }
-                    switch (level) {
-                        case LogLevel.Error:
-                            console.error(message);
-                            return;
-                        case LogLevel.Info:
-                            return;
-                        case LogLevel.Verbose:
-                            console.debug(message);
-                            return;
-                        case LogLevel.Warning:
-                            console.warn(message);
-                            return;
-                        default:
-                            return;
-                    }
-                }
-            }
-        }
-    });
-    instance.initialize()
-    return instance;
-}
+export const useMsalInstance = function (
+  auth_config: BrowserAuthOptions,
+  cache_config: CacheOptions
+) {
+  const instance = new PublicClientApplication({
+    auth: auth_config,
+    cache: cache_config,
+    system: {
+      loggerOptions: {
+        loggerCallback: (
+          level: LogLevel,
+          message: string,
+          containsPii: any
+        ) => {
+          if (containsPii) {
+            return;
+          }
+          switch (level) {
+            case LogLevel.Error:
+              console.error(message);
+              return;
+            case LogLevel.Info:
+              return;
+            case LogLevel.Verbose:
+              console.debug(message);
+              return;
+            case LogLevel.Warning:
+              console.warn(message);
+              return;
+            default:
+              return;
+          }
+        },
+      },
+    },
+  });
+  instance.initialize();
+  return instance;
+};
